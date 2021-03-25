4d0d9e0b91113015cb91ac7aba24d193ab72959d
Augusto Morais <augustofmorais@gmail.com>
2021-03-26 00:13:59 +0100

    chore: include first unitest

---
+++ tests/test_ask_questions.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import pytest
+from src.config.helper import replace_env_var
+
+def test_replace_env_var():
+    os.environ["HOME"] = "/home/FAKEUSER"
+    output = replace_env_var("$HOME/temp_dir")
+
+    assert output == "/home/FAKEUSER/temp_dir"
+    assert output != "$HOME/temp_dir"
